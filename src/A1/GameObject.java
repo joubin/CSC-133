8f340e319b0e0b141c69e04a1400f6ad4e3d0207
Joubin <joubin.j@gmail.com>
2013-10-01 22:19:57 -0700

    Adding comments to the code

--- src/A1/GameObject.java
+++ src/A1/GameObject.java
@@ -11,8 +11,11 @@ import java.util.Random;
  */
 public abstract class GameObject {
 
-    private float x, y;
-    Random myRandom = new Random();
+    private float x, y; // All game objects have an x and y coordinates
+    Random myRandom = new Random(); // A random number generator that will be used to set the inital positions of x and y
+    /*
+    For each item, get a random RGB value and create a color out of it using the color class.
+     */
     private float red = myRandom.nextFloat();
     private float green = myRandom.nextFloat();
     private float blue = myRandom.nextFloat();
@@ -20,29 +23,41 @@ public abstract class GameObject {
     private Color myColor = new Color(red, green, blue);
 
 
-
+    /*
+    Returns the X location of an item.
+     */
     public float getX() {
-
         return x;
     }
-
+    /*
+    Setting the x location. The formula below ensures that the the range is always positive
+     */
     public void setX(float x) {
-        this.x = ((x % 1023 + 1023) % 1023);
+        this.x = ((x % 1024 + 1024) % 1024);
     }
-
+    /*
+    Returns the y location of an item.
+    */
     public float getY() {
         return y;
     }
-
+    /*
+    Setting the y location. The formula below ensures that the the range is always positive
+     */
     public void setY(float y) {
         this.y = ((y % 1023 + 1023) % 1023);
     }
 
-
+    /*
+    Returns the color of the given object
+     */
     public Color getMyColor() {
         return myColor;
     }
-
+    /*
+    To string that all objects have in common.
+    Returns a string consiting of the location, and color of the object
+     */
     public String toString(){
         return String.format("loc=[%.2f,%.2f] color=[%s,%s,%s]", x,y,myColor.getRed(), myColor.getGreen(), myColor.getBlue());
     }
--- src/A1/MovableItem.java
+++ src/A1/MovableItem.java
@@ -7,52 +7,90 @@ package A1;
  * Time: 11:43 PM
  */
 public abstract class MovableItem extends GameObject {
-    private int direction;
-    private int speed;
-    private int health;
+    /*
+    Movable game objects
+     */
+    private int direction;  // Direction of the object
+    private int speed;  // Speed of the object
+    private int health; // Health of the object
 
     public MovableItem(int direction){
+        /*
+        All objects have to have a direction when first constructed.
+         */
         this.direction = direction;
     }
 
     public int getHealth() {
+        /*
+        returns the health of each object.
+
+        Different objects have different health parameters.
+         */
         return health;
     }
 
     public void setHealth(int health) {
+        /*
+        Increase or decrease the health.
+
+        Increase: increase the object obtains a lifepack or a boost
+        DecreaseL decrease if the object is hit or is part of its lifecycle.
+         */
         this.health += health;
     }
 
 
     public int getSpeed() {
+        /*
+        returns the speed of the objects. All movable objects have a starting speed.
+         */
         return speed;
     }
 
     public void setSpeed(int speed) {
+        /*
+        set the speed of the object
+         */
         this.speed = speed;
     }
 
 
     public int getDirection() {
+        /*
+        Return the direction of the item
+         */
         return direction;
     }
 
     private void setDirection(int direction){
+        /*
+        set a new direction for a given object
+         */
         this.direction = ((direction % 360 + 360) % 360);
     }
 
     public void mChangeDirection(int direction) {
-        if(this instanceof ISteerable){
+        /*
+        change direction. Only some movable object can change their course.
+         The commented out section below further insures that rule.
+
+         However it is not a requirement as long as sub classes are properly implemented.
+         */
+//        if(this instanceof ISteerable){
             if (direction % 5 == 0) {
                 setDirection(getDirection() + direction);
             } else {
                 int d = getDirection() - direction;
                 setDirection(getDirection() - (direction%5));
             }
-        }
+//        }
     }
 
     public void move() {
+        /*
+        This move method properly moves the objects given the correct direction and speed
+         */
         float newx = (float) Math.sin(direction) * speed;
         float newy = (float) Math.cos(direction) * speed;
         newx += getX();
@@ -62,6 +100,14 @@ public abstract class MovableItem extends GameObject {
     }
 
     public void update(){
+        /*
+        added for ease of use.
+
+        Could call move() directly instead.
+
+        However, the word move does not fully collect all of the tasks that need to take place at each
+        game tick.
+         */
         move();
     }
 
